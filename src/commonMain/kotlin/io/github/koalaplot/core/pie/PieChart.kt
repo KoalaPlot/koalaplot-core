ddbbc129e4064329f33d90dee07acf43d614104f
Maik Buchmeyer <maik@mediavrog.net>
2024-08-25 16:02:58 +0200

    fix missing import

--- src/commonMain/kotlin/io/github/koalaplot/core/pie/PieChart.kt
+++ src/commonMain/kotlin/io/github/koalaplot/core/pie/PieChart.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
