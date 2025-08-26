4014a00acf623e6c891e883940b89c57b4b0e5df
Greg Steckman <27028452+gsteckman@users.noreply.github.com>
2025-08-25 20:42:31 -0700

    Update for sonatype changes.

--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -96,8 +96,8 @@ publishing {
 nexusPublishing {
     repositories {
         sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
             stagingProfileId.set(getExtraString("sonatypeStagingProfileId"))
             username.set(getExtraString("ossrhUsername"))
             password.set(getExtraString("ossrhPassword"))
